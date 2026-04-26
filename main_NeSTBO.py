f3cf52adbbe3bfc8c0fdc61e3b6141b319ba05af
Wei-Ting Tang <jontwt@ascend-rw02.ten.osc.edu>
2026-04-26 11:10:06 -0400

    update

--- main_NeSTBO.py
+++ main_NeSTBO.py
@@ -32,6 +32,6 @@ def main(config: DictConfig) -> None:
 
 
 if __name__ == "__main__":
-    import sys
-    sys.argv.append("benchmark=swimmer")
+    # import sys
+    # sys.argv.append("benchmark=swimmer")
     main()
